e41c252e470681cd1c91451af217d282bd65554d
Timothy Axford <t.axford2@yahoo.com>
2023-03-29 14:07:11 -0400

    Using the Peek() method to return the value at the top of the stack.

--- Stacks.Net5.0/Program.cs
+++ Stacks.Net5.0/Program.cs
@@ -11,6 +11,8 @@ namespace Stacks.Net5._0
             Stack<int> stack = new Stack<int>();
             //add elements to the stack using Push()
             stack.Push(1);
+            //Peek() will return the element at the top of the stack without removing it
+            Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
         }
     }
 }
