8dab6ae1fa4ee62f7b31eaf8ec6f8a144174d668
Timothy Axford <t.axford2@yahoo.com>
2023-03-29 14:11:39 -0400

    Added an example of stock.pop to remove an item from the stack

--- Stacks.Net5.0/Program.cs
+++ Stacks.Net5.0/Program.cs
@@ -11,6 +11,9 @@ namespace Stacks.Net5._0
             Stack<int> stack = new Stack<int>();
             //add elements to the stack using Push()
             stack.Push(1);
+            //remove item from a stack
+            int myStackItem = stack.Pop();
+            stack.Push(1337);
             //Peek() will return the element at the top of the stack without removing it
             Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
         }
